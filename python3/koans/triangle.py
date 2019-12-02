3014f129f399dd5a14745cb82453ab0bdf77c271
Michael T <bravmech@gmail.com>
2019-12-02 12:36:51 +0500

    triangle done

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,13 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    unique = len({a, b, c})
+    if unique == 1:
+        return 'equilateral'
+    elif unique == 2:
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
