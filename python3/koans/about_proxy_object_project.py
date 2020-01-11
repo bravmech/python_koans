688348a604a5e96fb9ba5e13d7f1eb4761dd8136
Michael T <bravmech@gmail.com>
2020-01-11 15:42:42 +0500

    fixed an attribute bug

--- python3/koans/about_proxy_object_project.py
+++ python3/koans/about_proxy_object_project.py
@@ -36,14 +36,15 @@ class Proxy:
         return getattr(self._obj, name)
 
     def __setattr__(self, name, value):
-        object.__setattr__(self, name, value)
         self._messages.append(name)
+        setattr(self._obj, name, value)
 
     def was_called(self, name):
         return name in self._messages
 
     def number_of_times_called(self, name):
         return self._messages.count(name)
+
 
 # The proxy object should pass the following Koan:
 #
